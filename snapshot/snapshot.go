999795459ae6d3e79e44f547cc71904faafe7da5
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-02-17 23:50:04 +0800

    feat: Generate out a pretty json output for the snapshot function

--- snapshot/snapshot.go
+++ snapshot/snapshot.go
@@ -1,11 +1,21 @@
 package snapshot
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"google.golang.org/api/analytics/v3"
 )
+
+type snapshot struct {
+	Profiles         []*analytics.Profile
+	Filters          []*analytics.Filter
+	FilterLinks      []*analytics.ProfileFilterLink
+	Goals            []*analytics.Goal
+	CustomDimensions []*analytics.CustomDimension
+	CustomMetrics    []*analytics.CustomMetric
+}
 
 func getManagementService(client *http.Client) *analytics.ManagementService {
 	analyticsService, _ := analytics.New(client)
@@ -14,23 +24,52 @@ func getManagementService(client *http.Client) *analytics.ManagementService {
 }
 
 // GetSnapshot exported function
-func GetSnapshot(client *http.Client, gaAccountID string, gaPropertyID string, gaViewID string) {
+func GetSnapshot(client *http.Client, gaAccountID string, gaPropertyID string, gaViewID string) []byte {
 	mgmtService := getManagementService(client)
-
-	filters, err := mgmtService.Filters.List(gaAccountID).Do()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	data, _ := filters.MarshalJSON()
-	fmt.Println(string(data))
 
 	profiles, err := mgmtService.Profiles.List(gaAccountID, gaPropertyID).Do()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	data, err = profiles.MarshalJSON()
+
+	filters, err := mgmtService.Filters.List(gaAccountID).Do()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	fmt.Println(string(data))
+
+	filterLinks, err := mgmtService.ProfileFilterLinks.List(gaAccountID, gaPropertyID, gaViewID).Do()
+	if err != nil {
+		fmt.Println("Error during profile filter link extraction")
+		fmt.Println(err.Error())
+	}
+
+	goals, err := mgmtService.Goals.List(gaAccountID, gaPropertyID, gaViewID).Do()
+	if err != nil {
+		fmt.Println("Error during goals extraction")
+		fmt.Println(err.Error())
+	}
+
+	customDimensions, err := mgmtService.CustomDimensions.List(gaAccountID, gaPropertyID).Do()
+	if err != nil {
+		fmt.Println("Error during Custom Dimensions extraction")
+		fmt.Println(err.Error())
+	}
+
+	customMetrics, err := mgmtService.CustomMetrics.List(gaAccountID, gaPropertyID).Do()
+	if err != nil {
+		fmt.Println("Error during Custom Metrics extraction")
+		fmt.Println(err.Error())
+	}
+
+	currentSnapshot := snapshot{
+		profiles.Items,
+		filters.Items,
+		filterLinks.Items,
+		goals.Items,
+		customDimensions.Items,
+		customMetrics.Items,
+	}
+
+	output, err := json.MarshalIndent(currentSnapshot, "", "\t")
+	return output
 }
