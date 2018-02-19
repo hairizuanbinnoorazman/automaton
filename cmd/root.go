4285e4e401d3e6fc985759cad5e9752988438b8e
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-02-19 11:02:59 +0800

    feat: Rearrangement of flags from global flags to localized flags

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/hairizuanbinnoorazman/automaton/guide"
 	"github.com/hairizuanbinnoorazman/automaton/snapshot"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -50,15 +51,49 @@ var (
 			fmt.Println(string(snapshotData))
 		},
 	}
+
+	guideConfig string
+	guideCmd    = &cobra.Command{
+		Use:   "guide",
+		Short: "Use this set of commands to assist in creating guides on usage of marketing tools",
+		Long:  `Not available yet`,
+	}
+
+	guideInitCmd = &cobra.Command{
+		Use:   "init",
+		Short: "Use this command to initialize the required json files for the guide you intend to create",
+		Long:  `Not available yet`,
+		Run: func(cmd *cobra.Command, args []string) {
+			guideConfig := guide.NewGuideConfig()
+			fmt.Println(string(guideConfig))
+		},
+	}
+
+	guideGenerateCmd = &cobra.Command{
+		Use:   "generate",
+		Short: "Use this command to generate the output based on configurations and lists",
+		Long:  "The generate command allows the creation of an Implementation Guide. The creation of the guide is dependent on the usage of markdown file and csv files.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Generating a GTM implementation Guide")
+			guide.GenerateGuide(guideConfig)
+			fmt.Println("File generated. Check output file that was declared in your config file.")
+		},
+	}
 )
 
 func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.AddCommand(versionCmd)
+
 	rootCmd.AddCommand(snapshotCmd)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.json", "Default config file is config.yaml")
-	rootCmd.PersistentFlags().StringVar(&credFile, "cred", "cred.json", "Default config file is cred.yaml")
+	snapshotCmd.Flags().StringVar(&cfgFile, "config", "config.json", "Default config file is config.yaml")
+	snapshotCmd.Flags().StringVar(&credFile, "cred", "cred.json", "Default config file is cred.yaml")
+
+	rootCmd.AddCommand(guideCmd)
+	guideCmd.AddCommand(guideInitCmd)
+	guideCmd.AddCommand(guideGenerateCmd)
+	guideGenerateCmd.Flags().StringVar(&guideConfig, "config", "config.json", "Default config file is config.json")
 }
 
 // Execute cli commands
