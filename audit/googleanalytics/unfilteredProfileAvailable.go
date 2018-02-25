6b06cc89a6da45787b37d95bae6d25ed4cdfbf13
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-02-25 22:22:07 +0800

    chore: Rename files from just profile to the name of the audit

--- audit/googleanalytics/unfilteredProfileAvailable.go
+++ audit/googleanalytics/unfilteredProfileAvailable.go
@@ -1,11 +1,6 @@
 package googleanalytics
 
 import (
-	"fmt"
-	"io"
-	"path"
-	"text/template"
-
 	analytics "google.golang.org/api/analytics/v3"
 )
 
@@ -29,20 +24,6 @@ func (a *UnfilteredProfileAvailable) RunAudit() error {
 	a.Result = UnfilteredProfileAvailableResult{
 		ProfileCount:               2,
 		UnfilteredProfileAvailable: false}
-	return nil
-}
-
-func (a *UnfilteredProfileAvailable) RenderOutput(w io.Writer, templateFile string) error {
-	_, templateFileValue := path.Split(templateFile)
-	t := template.Must(template.New(templateFileValue).ParseFiles(templateFile))
-	err := t.Execute(w, a)
-	if err != nil {
-		fmt.Println("Unable to render template")
-		fmt.Println(err.Error())
-		return err
-	}
-	// Add a few new lines
-	io.WriteString(w, "\n\n")
 	return nil
 }
 
