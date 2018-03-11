d81f75feb8ebc60157dceb95661744ced3cdbca3
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-03-12 00:34:58 +0800

    feat: Completely move the current way of data extraction to a new separate way of doing things - use interfaces and using empty structs with functions attached to them as the implementation version of it - only need to follow the signature to get the analysis

--- audit/googleanalytics/dataExtract.go
+++ audit/googleanalytics/dataExtract.go
@@ -8,98 +8,100 @@ import (
 	analyticsreporting "google.golang.org/api/analyticsreporting/v4"
 )
 
-// DataExtractor interface.
+// GaMgmtExtractor interface.
 // This is the generic interface to ensure that the data being passed around the google analytics methods
 // In order to allow differing behaviours during runtime, we would allow the user to provide the client
 // struct which would alter the behaviour of the struct - Providing a real client would lead to calling
 // APIs but it also becomes possible to provide mocks for testing
-type DataExtractor interface {
-	Extract(client *http.Client) error
+type GaMgmtExtractor interface {
+	Extract(client *http.Client,
+		accountID string,
+		propertyID string,
+		profileID string,
+		mgmtItems []string) (GaMgmtProperties, error)
 }
 
-// GaMgmtParams struct is the parameters which is used to extract management settings data from
-// Google Analytics settings
-type GaMgmtParams struct {
-	AccountID  string
-	PropertyID string
-	ProfileID  string
-	MgmtItems  []string
-}
-
-// GaMgmtExtractor struct is the main struct that would be used to store information after the extract has been
+// GaMgmtExtract struct is the main struct that would be used to store information after the extract has been
 // extracted
-type GaMgmtExtractor struct {
-	Params  GaMgmtParams
-	Results GaMgmtProperties
-}
+type GaMgmtExtract struct{}
 
 // Extract function attached to the GaMgmtExtractor struct.
 // This function would extract the Google Analytics data and then store it into the internal dataset
 // Data is not returned but instead pulled out of the initialized struct
-func (e *GaMgmtExtractor) Extract(client *http.Client) error {
-	accountID := e.Params.AccountID
-	propertyID := e.Params.PropertyID
-	profileID := e.Params.ProfileID
+func (e *GaMgmtExtract) Extract(client *http.Client, accountID string,
+	propertyID string, profileID string, mgmtItems []string) (GaMgmtProperties, error) {
 
+	// Define Management Data Extraction Service
 	mgmtService := getManagementService(client)
 
-	for _, item := range e.Params.MgmtItems {
+	// Define struct to store all data
+	mgmtProperty := GaMgmtProperties{}
+
+	for _, item := range mgmtItems {
 		if item == profiles {
 			profileData, err := mgmtService.Profiles.List(accountID, propertyID).Do()
 			if err != nil {
-				return err
+				return GaMgmtProperties{}, err
 			}
-			e.Results.Profiles = profileData.Items
+			mgmtProperty.Profiles = profileData.Items
 		}
 		if item == goals {
 			goalData, err := mgmtService.Goals.List(accountID, propertyID, profileID).Do()
 			if err != nil {
-				return err
+				return GaMgmtProperties{}, err
 			}
-			e.Results.Goals = goalData.Items
+			mgmtProperty.Goals = goalData.Items
 		}
 		if item == profileFilterLinks {
 			profileFilterLinksData, err := mgmtService.ProfileFilterLinks.List(accountID, propertyID, profileID).Do()
 			if err != nil {
-				return err
+				return GaMgmtProperties{}, err
 			}
-			e.Results.ProfileFilterLinks = profileFilterLinksData.Items
+			mgmtProperty.ProfileFilterLinks = profileFilterLinksData.Items
 		}
 	}
-	return nil
+	return mgmtProperty, nil
 }
 
-// GaDataParams is the struct for the parameters of the Google Analytics Data extraction
-type GaDataParams struct {
-	ReportRequest map[string][]*analyticsreporting.ReportRequest
+// GaDataExtractor interface is an interface which requires a struct to use a slightly altered version of the
+// data extractor interface.
+type GaDataExtractor interface {
+	Extract(client *http.Client,
+		reportRequest map[string][]*analyticsreporting.ReportRequest) (map[string][]*analyticsreporting.GetReportsResponse,
+		error)
 }
 
-// GaDataExtractor is the struct for managing the Google Analytics Data extraction method
-type GaDataExtractor struct {
-	Params  GaDataParams
-	Results map[string][]*analyticsreporting.GetReportsResponse
-}
+// GaDataExtract is the struct for managing the Google Analytics Data extraction method
+type GaDataExtract struct{}
 
 // Extract function attached to the GaDataExtractor struct.
 // This function would extract the Google Analytics data and then store it into the internal dataset
 // Data is not returned but instead should be pulled out of the initialized struct
-func (e *GaDataExtractor) Extract(client *http.Client) error {
+func (e *GaDataExtract) Extract(client *http.Client,
+	reportRequest map[string][]*analyticsreporting.ReportRequest) (map[string][]*analyticsreporting.GetReportsResponse,
+	error) {
+
+	// Initialize the results map
+	results := make(map[string][]*analyticsreporting.GetReportsResponse)
+
+	// Initialize data service
 	dataService := getGADataService(client)
 
-	for name, req := range e.Params.ReportRequest {
+	// Iterate through report request and store the data accordingly
+	for name, req := range reportRequest {
 		reportReq := analyticsreporting.GetReportsRequest{
 			ReportRequests: []*analyticsreporting.ReportRequest{req[0]},
 		}
 		response, err := dataService.Reports.BatchGet(&reportReq).Do()
 		if err != nil {
 			fmt.Println(err.Error())
-			return err
+			return nil, err
 		}
 		if response.HTTPStatusCode != 200 {
-			return errors.New("Unable to get values")
+			return nil, errors.New("Unable to get values")
 		}
 
-		e.Results[name] = append(e.Results[name], response)
+		results[name] = append(results[name], response)
 	}
-	return nil
+	return results, nil
 }
