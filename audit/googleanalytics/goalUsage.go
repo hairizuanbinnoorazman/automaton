8e29455d6a39007a484a5eaf5d57e92ea4b0677c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-03-12 00:32:27 +0800

    feat: Moved the current main goal usage analysis to be able to make use of google analytics data as well

--- audit/googleanalytics/goalUsage.go
+++ audit/googleanalytics/goalUsage.go
@@ -1,18 +1,24 @@
 package googleanalytics
 
 import (
+	"fmt"
+
 	analytics "google.golang.org/api/analytics/v3"
+	analyticsreporting "google.golang.org/api/analyticsreporting/v4"
 )
 
 // GoalUsageData defines the list of data points needed to run this audit.
 // It may include both management google analytics or data from the google analytics profile
 type GoalUsageData struct {
-	Goals []*analytics.Goal
+	Goals       []*analytics.Goal
+	GoalsGaData map[string][]*analyticsreporting.GetReportsResponse
+	auditDetails
 }
 
 // GoalUsageResult defines the list of data points that will be provided after running this audit
 type GoalUsageResult struct {
-	GoalCount int
+	GoalCount           int
+	GoalGaDataCollected map[string]bool
 }
 
 // GoalUsage is the main object which will interface the data and result structs and provide the RunAudit functionality to run
@@ -22,11 +28,70 @@ type GoalUsage struct {
 	Result   GoalUsageResult
 }
 
-// RunAudit method runs the analysis on the dataset to determine the status of the Goal Usage in the profile
-func (a *GoalUsage) RunAudit() error {
-	a.Result = GoalUsageResult{
-		GoalCount: len(a.Data.Goals),
+// Do method runs the analysis on the dataset to determine the status of the Goal Usage in the profile
+func (a *GoalUsage) Do(mgmtExtractor GaMgmtExtractor, dataExtractor GaDataExtractor) error {
+	// Validate input data
+	a.Data.validate()
+
+	// Extract the GA Management Data
+	mgmtData, err := mgmtExtractor.Extract(a.Data.auditDetails.mgmtClient, a.Data.auditDetails.AccountID, a.Data.auditDetails.PropertyID,
+		a.Data.auditDetails.ProfileID, a.Metadata.DataExtractors.GaMgmtProperties)
+	if err != nil {
+		return err
 	}
+
+	// Assign required data to Goals
+	a.Data.Goals = mgmtData.Goals
+
+	// Initialize the goals ga data map
+	a.Data.GoalsGaData = make(map[string][]*analyticsreporting.GetReportsResponse)
+
+	// Calculate number of goals used
+	a.Result.GoalCount = len(a.Data.Goals)
+
+	// Fetching raw GA goals data from Google analytics
+	for _, singleGoal := range a.Data.Goals {
+		goalExtractionString := fmt.Sprintf("ga:goal%sStarts", singleGoal.Id)
+
+		request := map[string][]*analyticsreporting.ReportRequest{
+			goalExtractionString: []*analyticsreporting.ReportRequest{
+				&analyticsreporting.ReportRequest{
+					DateRanges: []*analyticsreporting.DateRange{
+						&analyticsreporting.DateRange{
+							StartDate: a.Data.StartDate,
+							EndDate:   a.Data.EndDate,
+						},
+					},
+					ViewId: a.Data.ProfileID,
+					Dimensions: []*analyticsreporting.Dimension{
+						&analyticsreporting.Dimension{
+							Name: "ga:date",
+						},
+					},
+					Metrics: []*analyticsreporting.Metric{
+						&analyticsreporting.Metric{
+							Expression: goalExtractionString,
+						},
+					},
+				},
+			},
+		}
+
+		value, err := dataExtractor.Extract(a.Data.dataClient, request)
+		if err != nil {
+			return err
+		}
+		a.Data.GoalsGaData[goalExtractionString] = append(a.Data.GoalsGaData[goalExtractionString], value[goalExtractionString][0])
+	}
+
+	// Initialize the goal ga data collected section
+	a.Result.GoalGaDataCollected = make(map[string]bool)
+
+	for goalName, singleGoalGaData := range a.Data.GoalsGaData {
+		goalUsed := len(singleGoalGaData) > 0
+		a.Result.GoalGaDataCollected[goalName] = goalUsed
+	}
+
 	return nil
 }
 
