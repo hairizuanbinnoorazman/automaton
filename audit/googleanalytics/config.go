60c0cecf35e391ba79b481a8248cd96f82a9331b
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-29 23:23:30 +0800

    fix: Split the custom dimensions and metrics file into two

--- audit/googleanalytics/config.go
+++ audit/googleanalytics/config.go
@@ -42,7 +42,8 @@ func NewConfig() Config {
 	var newAuditItems []auditItem
 	newAuditItems = append(newAuditItems, auditItem{Name: NewUnfilteredProfileAvailable().Metadata.Name, TemplateFile: createFileName(NewUnfilteredProfileAvailable().Metadata.Name)})
 	newAuditItems = append(newAuditItems, auditItem{Name: NewGoalUsage().Metadata.Name, TemplateFile: createFileName(NewGoalUsage().Metadata.Name)})
-	newAuditItems = append(newAuditItems, auditItem{Name: NewCustomDimMetricUsage().Metadata.Name, TemplateFile: createFileName(NewCustomDimMetricUsage().Metadata.Name)})
+	newAuditItems = append(newAuditItems, auditItem{Name: NewCustomDimUsage().Metadata.Name, TemplateFile: createFileName(NewCustomDimUsage().Metadata.Name)})
+	newAuditItems = append(newAuditItems, auditItem{Name: NewCustomMetricUsage().Metadata.Name, TemplateFile: createFileName(NewCustomMetricUsage().Metadata.Name)})
 
 	newConfig.AuditItems = newAuditItems
 	return newConfig
