ce50eeb101211bf33b23259ce58a3a237dae7dfb
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-30 00:51:16 +0800

    chore: New experiment - separate the current calling of services from the actual audit

---
+++ audit/googleanalytics/models/profile.go
@@ -0,0 +1,15 @@
+package googleanalytics
+
+import analytics "google.golang.org/api/analytics/v3"
+
+type profileAuditor struct {
+	Profiles           []*analytics.Profile
+	ProfileFilterLinks []*analytics.ProfileFilterLink
+}
+
+func (p profileAuditor) HasMoreThan1() bool {
+	if len(p.Profiles) > 1 {
+		return true
+	}
+	return false
+}
