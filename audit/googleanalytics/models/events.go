e80404b2daf31e2e11a779231b838fb48c2fb28a
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-14 00:07:32 +0800

    feat: Add events tracking - structs as well as functions to handle the model events

---
+++ audit/googleanalytics/models/events.go
@@ -0,0 +1,28 @@
+package googleanalytics
+
+type eventsAuditor struct {
+	events []eventItem
+}
+
+type eventItem struct {
+	eventCategory string
+	eventAction   string
+	eventLabel    string
+	eventValue    int
+}
+
+func (e eventsAuditor) HasMoreThan0() bool {
+	return len(e.events) > 0
+}
+
+func (e eventItem) InconsistentCaseEventCategory() bool {
+	return true
+}
+
+func (e eventItem) InconsistentCaseEventAction() bool {
+	return true
+}
+
+func (e eventItem) InconsistentCaseEventLabel() bool {
+	return true
+}
