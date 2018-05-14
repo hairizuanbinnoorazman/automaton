007416bef5df82a0efc92e08098a250a110544db
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-14 23:47:40 +0800

    chore: Add interface methods to extract data as one huge interface

--- audit/googleanalytics/googleanalyticsV2.go
+++ audit/googleanalytics/googleanalyticsV2.go
@@ -7,7 +7,12 @@ import (
 
 type Extractor interface {
 	GetCustomDimSettings(accountID, viewID, profileID string) ([]*analytics.CustomDimension, error)
-	GetCustomDimValues(profileID string) ([]models.CustomDimensionItem, error)
 	GetCustomMetricSettings(accountID, viewID, profileID string) ([]*analytics.CustomMetric, error)
+	GetGoalSettings(accountID, viewID, profileID string) ([]*analytics.Goal, error)
+	GetProfileSettings(accountID, viewID, profileID string) ([]*analytics.Profile, error)
+	GetProfileLinkSettings(accountID, viewID, profileID string) ([]*analytics.ProfileFilterLink, error)
+
+	GetCustomDimValues(profileID string) ([]models.CustomDimensionItem, error)
 	GetCustomMetricValues(profileID string) ([]models.CustomMetricsItem, error)
+	GetGoalValues(profileID string) ([]models.GoalItem, error)
 }
