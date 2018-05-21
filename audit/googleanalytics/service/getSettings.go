c5590ff8467ccf883464b0366dd7af1aac0c940a
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-21 12:34:46 +0800

    fix: Alter the function signatures to match the interface defined in the google anayltics v2 go file

--- audit/googleanalytics/service/getSettings.go
+++ audit/googleanalytics/service/getSettings.go
@@ -2,22 +2,27 @@ package service
 
 import analytics "google.golang.org/api/analytics/v3"
 
-func (s Service) GetCustomDimSettings(accountID, viewID, profileID string) ([]*analytics.CustomDimension, error) {
+func (s Service) GetCustomDimSettings(accountID, propertyID, profileID string) ([]*analytics.CustomDimension, error) {
 	return nil, nil
 }
 
-func (s Service) GetCustomMetricSettings(accountID, viewID, profileID string) ([]*analytics.CustomMetric, error) {
+func (s Service) GetCustomMetricSettings(accountID, propertyID, profileID string) ([]*analytics.CustomMetric, error) {
 	return nil, nil
 }
 
-func (s Service) GetGoalSettings(accountID, viewID, profileID string) ([]*analytics.Goal, error) {
+func (s Service) GetGoalSettings(accountID, propertyID, profileID string) ([]*analytics.Goal, error) {
 	return nil, nil
 }
 
-func (s Service) GetProfileSettings(accountID, viewID, profileID string) ([]*analytics.Profile, error) {
-	return nil, nil
+func (s Service) GetProfileSettings(accountID, propertyID, profileID string) ([]*analytics.Profile, error) {
+	mgmtService := s.getManagementService()
+	profileData, err := mgmtService.Profiles.List(accountID, propertyID).Do()
+	if err != nil {
+		return []*analytics.Profile{}, err
+	}
+	return profileData.Items, nil
 }
 
-func (s Service) GetProfileLinkSettings(accountID, viewID, profileID string) ([]*analytics.ProfileFilterLink, error) {
+func (s Service) GetProfileLinkSettings(accountID, propertyID, profileID string) ([]*analytics.ProfileFilterLink, error) {
 	return nil, nil
 }
