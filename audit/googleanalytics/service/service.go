8072a179f5097f467cb949b7391320883d997f76
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-21 12:46:39 +0800

    feat: Require a client variable that is to be passed in so that it can be used for the requests - this is done by embedding that variable in a struct - this is a request dependent struct

--- audit/googleanalytics/service/service.go
+++ audit/googleanalytics/service/service.go
@@ -1,3 +1,23 @@
 package service
 
-type Service struct{}
+import (
+	"net/http"
+
+	analytics "google.golang.org/api/analytics/v3"
+	analyticsreporting "google.golang.org/api/analyticsreporting/v4"
+)
+
+type Service struct {
+	Client *http.Client
+}
+
+func (s Service) getManagementService() *analytics.ManagementService {
+	analyticsService, _ := analytics.New(s.Client)
+	managementService := analytics.NewManagementService(analyticsService)
+	return managementService
+}
+
+func (s Service) getGADataService() *analyticsreporting.Service {
+	analyticsDataService, _ := analyticsreporting.New(s.Client)
+	return analyticsDataService
+}
