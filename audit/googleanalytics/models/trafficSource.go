1b8f424f3ef1678ed2816005b9f58737b2d7ee44
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-27 17:08:16 +0800

    feat: Add traffic source model and its audit skeletal functions

---
+++ audit/googleanalytics/models/trafficSource.go
@@ -0,0 +1,39 @@
+package models
+
+type TrafficSourceData struct {
+	Name                     string
+	Description              string
+	TrafficSources           []TrafficSourceItem
+	InconsistentCaseMedium   bool
+	InconsistentCaseSource   bool
+	InconsistentCaseCampaign bool
+}
+
+type TrafficSourceItem struct {
+	Medium   string
+	Source   string
+	Campaign string
+	Sessions int
+}
+
+func NewTrafficSourceData() TrafficSourceData {
+	return TrafficSourceData{Name: "test", Description: "test"}
+}
+
+func (t *TrafficSourceData) checkInconsistentCaseMedium() {
+	return
+}
+
+func (t *TrafficSourceData) checkInconsistentCaseSource() {
+	return
+}
+
+func (t *TrafficSourceData) checkInconsistentCaseCampaign() {
+	return
+}
+
+func (t *TrafficSourceData) RunAudit() {
+	t.checkInconsistentCaseMedium()
+	t.checkInconsistentCaseSource()
+	t.checkInconsistentCaseCampaign()
+}
